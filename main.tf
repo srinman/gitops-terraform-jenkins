30d8748c7932909c68acb4afc3c7b11237dfef7b
srinman <snmanivel@gmail.com>
2021-05-24 16:21:56 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,18 +13,7 @@ provider "aws" {
 }
 
 # Create EC2 instance
-resource "aws_instance" "default" {
-  ami                    = var.ami
-  count                  = var.instance_count
-  key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.default.id]
-  source_dest_check      = false
-  instance_type          = var.instance_type
 
-  tags = {
-    Name = "terraform-default"
-  }
-}
 
 # Create Security Group for EC2
 resource "aws_security_group" "default" {
