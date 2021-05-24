c239a90498d1132559f38f23cd7d26c511b40ca4
srinman <snmanivel@gmail.com>
2021-05-24 16:19:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group" "default" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
 }
