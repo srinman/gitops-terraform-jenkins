56a348fd075aa45e24c0708b0d9e725a5c9ea529
srinman <snmanivel@gmail.com>
2021-05-24 16:18:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,10 +38,10 @@ resource "aws_security_group" "default" {
   }
 
   ingress {
-    from_port   = 80
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/0"]
   }
 
 }
